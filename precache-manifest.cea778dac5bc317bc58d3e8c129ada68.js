441a162872315aff3eae08c65b4fb019f26e6fdc
Deyna <d.baeva@student.fontys.nl>
2020-10-06 23:32:09 +0200

    Updates

---
+++ precache-manifest.cea778dac5bc317bc58d3e8c129ada68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f7b752c9507876dce43cccca6e17bd",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "50821d7b3e0102a84be5",
+    "url": "/react-weather-app/static/css/main.3612223d.chunk.css"
+  },
+  {
+    "revision": "c15d4796229d148464a4",
+    "url": "/react-weather-app/static/js/2.b88b163c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.b88b163c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50821d7b3e0102a84be5",
+    "url": "/react-weather-app/static/js/main.68af4bdd.chunk.js"
+  },
+  {
+    "revision": "8939bd8769851b270234",
+    "url": "/react-weather-app/static/js/runtime-main.34187cdb.js"
+  },
+  {
+    "revision": "017413d7114634238131c99d06e2d37d",
+    "url": "/react-weather-app/static/media/cold.017413d7.jpg"
+  },
+  {
+    "revision": "e5a56a62365f26b632386f8005728d5a",
+    "url": "/react-weather-app/static/media/warm.e5a56a62.jpg"
+  }
+]);
